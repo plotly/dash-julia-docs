1db4e58735270a200240072958899e5077127507
Joseph Damiba <joseph.damiba@plot.ly>
2021-06-01 22:46:42 -0400

    add more rangeslider examples

---
+++ dash_docs/chapters/dash_core_components/RangeSlider/examples/pushable_handles.jl
@@ -0,0 +1,16 @@
+using Dash, DashHtmlComponents, DashCoreComponents
+
+
+app = dash()
+
+app.layout = html_div() do
+    dcc_rangeslider(
+        min=0,
+        max=30,
+        value=[8, 10, 15, 17, 20],
+        pushable=2
+    )
+end
+
+
+run_server(app, "0.0.0.0", debug=true)
--- dash_docs/chapters/dash_core_components/RangeSlider/index.jl
+++ dash_docs/chapters/dash_core_components/RangeSlider/index.jl
@@ -14,6 +14,10 @@ rangeslider2 = LoadExampleCode(string(examples_path, "/rangeslider_marks_steps.j
 
 rangeslider3 = LoadExampleCode(string(examples_path, "/rangeslider_styling.jl"))
 rangeslider4 = LoadExampleCode(string(examples_path, "/rangeslider_included_false.jl"))
+rangeslider5 = LoadExampleCode(string(examples_path, "/multiple_handles.jl"))
+rangeslider6 = LoadExampleCode(string(examples_path, "/pushable_handles.jl"))
+rangeslider7 = LoadExampleCode(string(examples_path, "/crossing_handles.jl"))
+
 
 examples = [rangeslider1]
 
@@ -47,7 +51,28 @@ app.layout = html_div() do
     rangeslider3.layout,
 
     rangeslider4.source_code,
-    rangeslider4.layout
+    rangeslider4.layout,
+
+    html_h3("Multiple Handles"),
+
+    dcc_markdown("To create multiple handles just define more values for `value`!"),
+
+    rangeslider5.source_code,
+    rangeslider5.layout,
+
+    html_h3("Pushable Handles"),
+
+    dcc_markdown("The `pushable` property is either a `boolean` or a numerical value. The numerical value determines the minimum distance between the handles. Try moving the handles around!"),
+
+    rangeslider6.source_code,
+    rangeslider6.layout,
+
+    html_h3("Crossing Handles"),
+
+    dcc_markdown("If `allowCross=false`, the handles will not be allowed to cross over each other"),
+
+    rangeslider7.source_code,
+    rangeslider7.layout
 
 
 end

c411c77636d4d156059052ba47351055ed25f6a4
Joseph Damiba <joseph.damiba@plot.ly>
2021-06-10 12:00:10 -0400

    add date format table

--- dash_docs/chapters/dash_core_components/DatePickerSingle/index.jl
+++ dash_docs/chapters/dash_core_components/DatePickerSingle/index.jl
@@ -25,7 +25,103 @@ app.layout = html_div() do
     If you already have a `DateTime` object, you can easily convert it with `Date()`. The `min_date_allowed` and `max_date_allowed` properties define the minimum and maximum selectable dates on the calendar while `initial_visible_month` defines the calendar month that is first displayed when the `DatePickerSingle` component is opened."""),
 
     simple_datepicker.source_code,
-    simple_datepicker.layout
+    simple_datepicker.layout,
+
+    html_h3("Month and Display Format"),
+
+    dcc_markdown("""
+    The `display_format` property determines how selected dates are displayed in the `DatePickerSingle` component. The `month_format` property determines how calendar headers are displayed when the calendar is opened.
+
+    Both of these properties are configured through strings that utilize a combination of any of the following tokens."""),
+
+    html_table([
+        html_tr(
+            [
+                html_th("String Token", style = Dict("text-align" => "left", "width" => "20%")),
+                html_th("Example", style = Dict("text-align" => "left", "width" => "20%")),
+                html_th("Description", style = Dict("text-align" => "left", "width" => "20%"))
+            ]
+        ),
+        html_tr(
+            [
+                html_td(dcc_markdown("`YYYY`")),
+                html_td(dcc_markdown("`2014`")),
+                html_td("4 or 2 digit year")
+            ]
+        ),
+        html_tr(
+            [
+                html_td(dcc_markdown("`YY`")),
+                html_td(dcc_markdown("`14`")),
+                html_td("2 digit year")
+            ]
+        ),
+        html_tr(
+            [
+                html_td(dcc_markdown("`Y`")),
+                html_td(dcc_markdown("`-25`")),
+                html_td("Year with any number of digits and sign")
+            ]
+        ),
+        html_tr(
+            [
+                html_td(dcc_markdown("`Q`")),
+                html_td(dcc_markdown("`1..4`")),
+                html_td("Quarter of year. Sets month to first month in quarter.")
+            ]
+        ),
+        html_tr(
+            [
+                html_td(dcc_markdown("`M MM`")),
+                html_td(dcc_markdown("`1..12`")),
+                html_td("Month number")
+            ]
+        ),
+        html_tr(
+            [
+                html_td(dcc_markdown("`MMM MMM`")),
+                html_td(dcc_markdown("`Jan..December`")),
+                html_td("Month name")
+            ]
+        ),
+        html_tr(
+            [
+                html_td(dcc_markdown("`D DD`")),
+                html_td(dcc_markdown("`1..31`")),
+                html_td("Day of month")
+            ]
+        ),
+        html_tr(
+            [
+                html_td(dcc_markdown("`Do`")),
+                html_td(dcc_markdown("`1st...31st`")),
+                html_td("Day of month with ordinal")
+            ]
+        ),
+        html_tr(
+            [
+                html_td(dcc_markdown("`DDD DDDD`")),
+                html_td(dcc_markdown("`1..365`")),
+                html_td("Day of year")
+            ]
+        ),
+        html_tr(
+            [
+                html_td(dcc_markdown("`X`")),
+                html_td(dcc_markdown("`1410715640.579`")),
+                html_td("Unix timestamp")
+            ]
+        ),
+        html_tr(
+            [
+                html_td(dcc_markdown("`x`")),
+                html_td(dcc_markdown("`1410715640579`")),
+                html_td("Unix ms timestamp")
+            ]
+        )]
+    ),
+
+    html_h3("Display Format Examples")
 
 end
 

81a20801ee95471bfadb984186ec5ac4b2f456ca
efmanu <manuedavakandam@gmail.com>
2021-08-06 02:09:03 +1000

    daqrefs

--- DashDocs/src/chapters/dash_daq/Joystick/index.jl
+++ DashDocs/src/chapters/dash_daq/Joystick/index.jl
@@ -43,33 +43,7 @@
       Enterprise's [Data Science Workspaces](https://plotly.com/dash/workspaces/),
        which has typeahead support for Dash Component Properties. [Find out if your company is using Dash Enterprise](https://go.plotly.com/company-lookup).
     """),
-    dcc_markdown("""
-    `id` (String; optional): The ID used to identify the color picker in Dash callbacks.
-
-    `angle` (Number; optional): Joystick angle in degrees, 0 = right, 90 = up, 180 = left, 270 = down.
-
-    `className` (String; optional): Class to apply to the root component element.
-
-    `disabled` (Bool; optional): If True, color cannot be picked.
-
-    `force` (Number; optional): Joystick force: distance between cursor and center in big-circle radii.
-
-    `label` (Dict; optional): Description to be displayed alongside the control. To control styling, pass an object with label and style properties.
-
-    `label` is a String | Dict with keys:
-
-        * `label` (String; optional)
-
-        * `style` (Dict; optional)
-
-    `labelPosition` (a value equal to: 'top', 'bottom'; default 'top'): Where the indicator label is positioned.
-
-    `size` (Number; default 100): Size (width) of the component in pixels.
-
-    `style` (Dict; optional): Style to apply to the root component element.
-
-    `theme` (Dict; default light): Theme configuration to be set by a ThemeProvider.
-    """)
+    dcc_markdown(string(@doc daq_joystick))
 
 end
 

d30119f532a2cbfbf7465dce1c967391d4216912
Spencer Lyon <spencerlyon2@gmail.com>
2021-08-11 13:38:01 -0400

    change (x|y)axis_title_text => (x|y)axis_title
    Needed downstream bug to be fixed in PlotlyBase -- https://github.com/sglyon/PlotlyBase.jl/commit/a82d8c3d13fbbbd5e911a805b0d40d6ca600c211

--- DashDocs/src/chapters/basic_callbacks/examples/hello-slider.jl
+++ DashDocs/src/chapters/basic_callbacks/examples/hello-slider.jl
@@ -7,14 +7,14 @@ years = unique(df1[!, :year])
 app = dash()
 
 app.layout = html_div() do
-    dcc_graph(id = "graph"),
+    dcc_graph(id="graph"),
     dcc_slider(
-        id = "year-slider-1",
-        min = minimum(years),
-        max = maximum(years),
-        marks = Dict([Symbol(v) => Symbol(v) for v in years]),
-        value = minimum(years),
-        step = nothing,
+        id="year-slider-1",
+        min=minimum(years),
+        max=maximum(years),
+        marks=Dict([Symbol(v) => Symbol(v) for v in years]),
+        value=minimum(years),
+        step=nothing,
     )
 end
 
@@ -26,19 +26,19 @@ callback!(
     return plot(
         df1[df1.year .== selected_year, :],
         Layout(
-            xaxis_type = "log",
-            xaxis_title_text = "GDP Per Capita",
-            yaxis_title_text = "Life Expectancy",
-            legend_x = 0,
-            legend_y = 1,
-            hovermode = "closest",
-            transition_duration = 500
+            xaxis_type="log",
+            xaxis_title="GDP Per Capita",
+            yaxis_title="Life Expectancy",
+            legend_x=0,
+            legend_y=1,
+            hovermode="closest",
+            transition_duration=500
         ),
-        x = :gdpPercap, y = :lifeExp,
-        text = :country, color = :continent,
-        mode = "markers",
-        marker=attr(size = 15, line_color="white"),
+        x=:gdpPercap, y=:lifeExp,
+        text=:country, color=:continent,
+        mode="markers",
+        marker=attr(size=15, line_color="white"),
     )
 end
 
-run_server(app, "0.0.0.0", debug = true)
+run_server(app, "0.0.0.0", debug=true)

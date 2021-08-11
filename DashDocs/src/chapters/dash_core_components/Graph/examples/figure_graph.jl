c0853cbea6101323b55f0fa250f941709fbe8c9f
Spencer Lyon <spencerlyon2@gmail.com>
2021-08-11 13:24:46 -0400

    re-add figure_graph, update Dash to 0.1.6

---
+++ DashDocs/src/chapters/dash_core_components/Graph/examples/figure_graph.jl
@@ -0,0 +1,16 @@
+using Dash, DashCoreComponents, PlotlyJS
+
+app = dash()
+
+app.layout = dcc_graph(
+  id="example-graph",
+  figure=Plot([
+      bar(x=1:3, y=[4, 1, 2], name="SF"),
+      bar(x=1:3, y=[2, 4, 5], name="Montréal"),
+  ], Layout(title="Dash Data Visualization")
+  )
+)
+
+run_server(app, "0.0.0.0", debug=true)
+
+

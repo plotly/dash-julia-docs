4c4713ce3a14cdeaa73453109c8bde2dfc34b706
Rohit Singh Rathaur <42641738+RohitRathore1@users.noreply.github.com>
2021-08-12 08:38:57 +0530

    fixed unwanted link issues in Reference section (#28)

--- DashDocs/src/chapters/dash_vtk/IntroVisualization/examples/geometry_rendering.jl
+++ DashDocs/src/chapters/dash_vtk/IntroVisualization/examples/geometry_rendering.jl
@@ -1,17 +1,19 @@
+## Work in Progress
+
 using Dash, DashHtmlComponents
 using DashVtk
 
 app = dash()
 
-content = vtk_view([
-    vtk_geometryrepresentation([
-        vtk_mesh(
-            spacing=[1, 1, 1],
-            dimensions=[10, 10, 10],
-            origin=[0, 0, 0],
-        )
-    ]),
-])
+#content = vtk_view([
+#    vtk_geometryrepresentation([
+#        vtk_imagedata(
+#            dimensions = [5, 5, 5],
+#            origin = [-2, -2, -2],
+#            spacing = [1, 1, 1],
+#    )
+#    ]),
+#])
 
 app.layout = html_div() do
     html_div(
